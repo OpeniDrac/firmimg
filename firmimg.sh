22f568f0cac2bd21f6d36734a90ff937157b7aa6
stidofficial <quentin.laffitte@gmail.com>
2018-04-08 23:54:28 +0200

    First commit

---
+++ firmimg.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# Firmware image format
+headerOffset=0
+headerSize=512
+headerReserved=512
+uImageOffset=$((headerOffset + headerReserved))
+uImageSize=4479904
+uImageReserved=4480000
+cramfsOffset=$((uImageOffset + uImageReserved))
+cramfsSize=52203520
+cramfsReserved=52203520
+unknownDataOffset=$((cramfsOffset + cramfsReserved))
+unknownDataSize=142904
+unknownDataReserved=142904
+firmimgOffset=$headerOffset
+firmimgSize=$((unknownDataOffset + unknownDataReserved))
+firmimgReserved=$((firmimgSize - 1))
+
+fileSize=`du -b firmimg.d6 |cut -f1`
+
+if [ $firmimgSize -eq $fileSize ]
+then
+	fileStatus="OK"
+else
+	fileStatus="FAILED"
+fi
+
+# Debug
+#binwalk firmimg.d6
+
+cat << EOF
+----------------------------------------
+Dell iDRAC Monolithic Release 2.90
+----------------------------------------
+Header offset		: $headerOffset
+Header size		: $headerSize
+uImage offset		: $uImageOffset
+uImage size		: $uImageSize
+cramfs offset		: $cramfsOffset
+cramfs size		: $cramfsSize
+Unknown data offset	: $unknownDataOffset
+Unknown data size	: $unknownDataSize
+Total size		: $firmimgSize
+
+File size		: $fileSize
+Firmware image status	: $fileStatus
+----------------------------------------
+EOF
+
+echo "Extracting firmware..."
+
+mkdir -p data
+
+# Extract header
+echo "[0/4] Extracting header..."
+dd if=firmimg.d6 of=data/header.bin bs=1 skip=$headerSize count=$headerSize status=progress
+echo "[1/4] Header extracted !"
+
+# Extract uImage
+echo "[1/4] Extracting uImage..."
+dd if=firmimg.d6 of=data/uImage bs=1 skip=$uImageOffset count=$uImageSize status=progress
+echo "[2/4] Complete !"
+
+# Extract cramfs
+echo "[2/4] Extracting cramfs..."
+dd if=firmimg.d6 of=data/cramfs bs=1 skip=$cramfsOffset count=$cramfsSize status=progress
+echo "[3/4] Complete !"
+
+# Extract unknown data
+echo "[3/4] Extracting unknown data..."
+dd if=firmimg.d6 of=data/unknown_data.bin bs=1 skip=$unknownDataOffset count=$unknownDataSize status=progress
+echo "[4/4] Complete !"
+
+echo "Firmware extracted !"
